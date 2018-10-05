594c4d76ec902450aabe2a009b6118f92213028d
Michael-Bianconi <michael.a.bianconi@gmail.com>
2018-10-05 15:11:43 -0400

    It's finished, just gonna go back and clean it up

--- KenKenCreator.java
+++ KenKenCreator.java
@@ -4,12 +4,14 @@
  * This KenKenCreator will create KenKen puzzles.
  *
  * $ javac KenKenCreator.java
- * $ java KenKenCreator #
+ * $ java KenKenCreator size filename
  *
- * where # is the size of the board.
  * For making not shitty puzzles, keep the size under 10. The program should
  * run fine for puzzles up to size 30. Beyond that it kinda shits itself under
  * the load.
+ *
+ * The output file is option. Leaving it blank or using the filename "stdout"
+ * have the puzzle printed to standard output.
  *
  * Step 1: Randomly fill the grid with 1-N unique values.
  * Step 2: Randomly generate regions around those values.
@@ -17,6 +19,7 @@
  *         non-negative integer target when applied to the
  *         values.
  * Step 4: Remove the values and leave the regions.
+ * Step 5: Write to the output file (or standard output)
  * Step 5: Cry
  *
  * In essence, this creator works backwards. It creates a solved puzzle,
@@ -32,22 +35,101 @@
  */
 
 import kenken.*;
+import java.io.*;
 import java.util.ArrayList;
 
 public class KenKenCreator {
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        int size = 0;
+        boolean solve = false;
+        String filename = "";
+
+        // parse input
+        if (args.length == 0 || args.length > 3) {
+            System.out.println("Usage: java KenKenCreator size [-s] [file]");
+            System.out.println("\tsize\tsize of puzzle");
+            System.out.println("\t-s\tshow solution to puzzle (optional)");
+            System.out.println("\tfile\toutput to this file (else print)");
+            return;
+        }
+
+        size = Integer.parseInt(args[0]);
+
+        if (args.length == 2) {
+            if (args[1].equals("-s")) {
+                solve = true;
+            }
+
+            else {
+                filename = args[1];
+            }
+        }
+
+        else if (args.length == 3) {
+            solve = true;
+            filename = args[2];
+        }
+
+        // redirect to file
+        if (filename != "") {
+            System.setOut(new PrintStream(new File(filename)));
+        }
+
+        // Generate the grid with values and region numbers
         Grid myKenKenBoard = new Grid(Integer.parseInt(args[0]));
         myKenKenBoard.generateValues();
         myKenKenBoard.generateRegions();
+
+        // Generate a list of all the regions
         Region[] regions = Region.readFromGrid(myKenKenBoard);
 
-        myKenKenBoard.printValues();
-        System.out.println("");
-        myKenKenBoard.printRegions();
+        // For each region, create and assign a target and operator
         for (Region r : regions) {
-            System.out.println(r.toString() + "\n");
+            r.generateOperatorAndTarget();
         }
+
+        writeToFile(myKenKenBoard, regions, solve);
+
     }
+
+    /**
+     * Given all the data, write it to a file. If the file does not exist,
+     * it is created. If the file is "stdout", simply print it. The data is
+     * written in the following format:
+     *
+     * 1 | [size] [num_regions]
+     * 2 |
+     * 3 | [nth region target][nth region operator] (for each region, 1-N)
+     * 4 |
+     * 5 | [region data array]
+     *
+     * Example:
+     *
+     *
+     * @param grid The data grid.
+     * @param regions Region data holding operators and targets.
+     * @param solution boolean, if true, print the solution at the third line.
+     */
+    public static void writeToFile(Grid grid,
+                                   Region[] regions,
+                                   boolean solution) {
+        System.out.printf("%d %d\n", grid.getSize(), grid.getNumRegions());
+        System.out.println("");
+
+        if (solution) {
+            grid.printValues();
+            System.out.println("");
+        }
+
+        for (Region r : regions) {
+            System.out.printf("%d %c\n", r.getTarget(), r.getOperator());
+        }
+
+        System.out.println("");
+        grid.printRegions();
+    }
+
 
 }
--- kenken/Region.java
+++ kenken/Region.java
@@ -6,6 +6,7 @@
 
 package kenken;
 
+import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -16,6 +17,7 @@ public class Region {
     private int target;
     private ArrayList<Integer> values;
 
+    //== CONSTRUCTORS ========================================================//
     /**
      * Constructor with only the region number.
      * @param region_number Identifier.
@@ -42,8 +44,10 @@ public class Region {
             this.values = (values == null) ? new ArrayList<Integer>() : values;
     }
 
+    //== GENERATORS ==========================================================//
     /**
-     * Given a Grid, read its region data into an Array.
+     * Given a Grid, read its region data into an Array of regions.
+     * Does NOT generate operators or targets.
      * @param grid Grid to read from
      * @return Array of regions with only values, no ops or targets.
      */
@@ -71,14 +75,122 @@ public class Region {
                     regions[r.getRegionNumber()-1] = r;
                 }
 
+                // existing region
                 else {
                     regions[r.getRegionNumber()-1].addValue(v);
                 }
             }
         }
-
-        System.out.println(regions == null);
         return regions;
+    }
+
+    /**
+     * Randomly choose an operator and attempt to create a valid target (non-
+     * negative integer). If impossible, choose a different operator. If
+     * region is of size 1, use the ' ' operator.
+     *
+     * Note: the division operator appears much less than the others, as it
+     *       is fairly uncommon for several random numbers to divide into
+     *       an integer.
+     */
+    public void generateOperatorAndTarget() {
+        Random rand = new Random();
+        ArrayList<Character> operations = new ArrayList<Character>();
+        operations.add('+');
+        operations.add('-');
+        operations.add('*');
+        operations.add('/');
+
+        // size 1 region:
+        if (this.values.size() == 1) {
+            this.operator = ' ';
+            this.target = this.values.get(0);
+            return;
+        }
+
+        while (operations.size() != 0) {
+
+            // Get and remove a random operation from the list
+            char op = operations.remove(rand.nextInt(operations.size()));
+            this.target = findTarget(op);
+
+            // we've found a valid target
+            if (this.target != -1) {
+                this.operator = op;
+                break;
+            }
+        }
+    }
+
+    /**
+     * Given an operator, see if it's possible to create a valid target.
+     * Will ALWAYS be valid if the operator is * or +.
+     * @param op operator to attempt
+     * @return the valid target, or -1
+     */
+    private int findTarget(char op) {
+
+        // addition operator, add all values and return
+        if (op == '+') {
+            int target = 0;
+            for (int v : this.values) {
+                target += v;
+            }
+            return target;
+        }
+
+        // multiplication operator, multiple all values and return
+        else if (op == '*') {
+            int target = 1;
+            for (int v : this.values) {
+                target *= v;
+            }
+            return target;
+        }
+
+        // subtraction operator, take largest value and subtract all others
+        // if the result is negative, return -1
+        else if (op == '-') {
+            int operand = 0;
+            int max = 0;
+
+            for (int v : this.values) {
+                if (v > max) {
+                    operand += max;
+                    max = v;
+                }
+
+                else {
+                    operand += v;
+                }
+            }
+
+            int target = max - operand;
+            return (target < 0) ? -1 : target;
+        }
+
+        // division operator, take the largest value and divide it by all
+        // the others. If the result isn't an integer, return -1.
+        else if (op == '/') {
+            int divisor = 1;
+            int max = 1;
+
+            for (int v : this.values) {
+                if (v > max) {
+                    divisor *= max;
+                    max = v;
+                }
+
+                else {
+                    divisor *= v;
+                }
+            }
+
+            // if there's no loss of precision, then it's an integer
+            return (max % divisor == 0) ? (int) (max / divisor) : -1;
+        }
+
+        return -1;
     }
 
     //== ACCESSORS ===========================================================//
@@ -97,6 +209,22 @@ public class Region {
      */
     public int getRegionNumber() {
         return this.region_number;
+    }
+
+    /**
+     * Get this region's target.
+     * @return int
+     */
+    public int getTarget() {
+        return this.target;
+    }
+
+    /**
+     * Get this region's operator.
+     * @return char
+     */
+    public char getOperator() {
+        return this.operator;
     }
 
 
